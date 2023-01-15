79c3082f45151cf4a6ecbb5bd6308e6ac86d906f
Franco Veron Peralta <francoveronperalta@gmail.com>
2023-01-15 16:30:54 -0300

    instalando cypress y testend

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
+// eslint-disable-next-line no-undef
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
-});
+})
